a15b387f1b77a9d8ee461ad1630e9977c517e2be
Estuardiaz2611 <44204205+Estuardiaz2611@users.noreply.github.com>
2019-07-30 14:13:02 -0600

    agregando videos

--- src/app/abdomen/abdomen.page.ts
+++ src/app/abdomen/abdomen.page.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-abdomen',
   templateUrl: 'abdomen.page.html',
   styleUrls: ['abdomen.page.scss']
 })
+
 export class AbdomenPage {
-  constructor() {
+  constructor(
+    private _sanitizer: DomSanitizer
+){}
+getVideoIframe(url) {
+  var video, results;
+
+  if (url === null) {
+      return '';
   }
+  results = url.match('[\\?&]v=([^&#]*)');
+  video   = (results === null) ? url : results[1];
+
+  return this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + video);   
+}
+
 }
