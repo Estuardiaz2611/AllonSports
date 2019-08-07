368213e0183a5ebe2c6778ca9d579d5769b59d25
Estuardiaz2611 <44204205+Estuardiaz2611@users.noreply.github.com>
2019-08-07 17:10:25 -0600

    errores

--- src/app/resultados/resultados.page.ts
+++ src/app/resultados/resultados.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Resultado } from '../models/resultados.models';
+import { ResultadoService } from '../services/resultado.service';
+import { GLOBAL } from '../services/global.service';
 
 
 @Component({
@@ -10,10 +12,34 @@ import { Resultado } from '../models/resultados.models';
 export class ResultadosPage {
   public url;
   public status;
+  public token;
+  public identity;
 
   // VARIABLES DE ENCUESTAS
   public resultados: Resultado[];
   public modelResultados: Resultado;
-  constructor() {}
+  constructor(
+    private _resultadoServices: ResultadoService,
+    ) {
+    this.url = GLOBAL.url;
+    this.modelResultados = new Resultado('', '', '', '');
+  }
+  
+  ngOnInit(){
+    this.getResultados();
+  }
 
+  getResultados(){
+    this._resultadoServices.getResultados(this.token).subscribe(response => {
+      if (response.resultado) {
+        this.modelResultados = response.resultado;
+        this.status="Ok"
+      } 
+    }, error => {
+      let errorMessage = <any>error;
+      console.log(errorMessage);
+      if (errorMessage != null)
+        this.status = 'error';
+    }
+  }
 }
